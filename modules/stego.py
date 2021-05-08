96e41601558ee5d61a84dadc6efefcda65b8aa48
Quotoms <azo.cw@yandex.ru>
2021-05-08 16:32:08 +0300

    stego stubs added

---
+++ modules/stego.py
@@ -0,0 +1,37 @@
+import os, sys
+
+
+class Steganography:
+    def __init__(self, degree, i, n):
+        self.DEGREE = degree
+        self.I = i
+        self.N = n
+        self.BMP_HEADER_SIZE = 54
+        self.FRAME_ARRAY = []
+
+    def encode_video(self):
+        # extract frames
+
+        # create video
+
+        # compile audio
+        pass
+
+    def encode_image(self):
+        pass
+
+    def encode_properties(self):
+        pass
+
+    def decode_video(self):
+        pass
+
+    def decode_image(self):
+        pass
+
+    def decode_properties(self):
+        pass
+
+    def get_masks(self):
+        pass
+
